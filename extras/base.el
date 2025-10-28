137b1925ccc6f9990a56b28c8a692fce96fb97a6
Ashton Wiersdorf <mail@wiersdorf.dev>
2025-10-28 10:33:26 -0600

    Don't need corfu-term in Emacs 31! Yay child frames in TTY mode!

--- extras/base.el
+++ extras/base.el
@@ -138,13 +138,6 @@
   :config
   (corfu-popupinfo-mode))
 
-;; Make corfu popup come up in terminal overlay
-(use-package corfu-terminal
-  :if (not (display-graphic-p))
-  :ensure t
-  :config
-  (corfu-terminal-mode))
-
 ;; Fancy completion-at-point functions; there's too much in the cape package to
 ;; configure here; dive in when you're comfortable!
 (use-package cape
