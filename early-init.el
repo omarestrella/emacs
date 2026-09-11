f5d1b06b14b8e10209dc7d3195e87acfebfcfa95
Omar Estrella <omarestrella@bitcreative.net>
2026-09-11 09:50:27 -0400

    early-init: drop forced dark frame colors; auto-dark drives appearance

--- early-init.el
+++ early-init.el
@@ -38,7 +38,8 @@
 (when (fboundp 'tool-bar-mode) ; When in a GUI, disable tool bar;
   (tool-bar-mode -1))          ; all these tools are in the menu-bar anyway
 
-;; These settings apply to *all* frames.
+;; These settings apply to *all* frames.  ns-appearance is left unset so
+;; auto-dark (extras/ide.el) can drive it from the OS appearance.
 (setq default-frame-alist '(
                             ;; Default window size, in characters
                             (width . 140)
@@ -46,20 +47,15 @@
                             ;; You can turn off scroll bars by uncommenting these lines:
                             ;; (vertical-scroll-bars . nil)
                             ;; (horizontal-scroll-bars . nil)
-                            (ns-appearance . dark)
-                            (ns-transparent-titlebar . t)
 
                             ;; Use this to turn off the OS window decoration
                             ;; (undecorated-round . t)
                             ;; (internal-border-width . 3)
                             ))
 
-;; These settings apply to the first frame created. The
-;; (back|fore)ground-color settings need to live here so that a
-;; theme's background color applies correctly to subsequent frames.
+;; These settings apply to the first frame created.  Colors start neutral
+;; (light); auto-dark re-paints them when the OS says dark.
 (setq initial-frame-alist '((width . 140)
                             (height . 42)
-                            ;; Setting the face in here prevents flashes of
-                            ;; color as the theme gets activated
-                            (background-color . "#000000")
-                            (foreground-color . "#ffffff")))
+                            (background-color . "#faf4ed")
+                            (foreground-color . "#575279")))
